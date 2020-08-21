961aea2eed80d0b3d51dbe8d4593c3b44f10523e
CaptainPMP <scikung8@gmail.com>
2020-08-21 21:10:42 +0700

    add some style

--- app.js
+++ app.js
@@ -30,9 +30,13 @@ function CalculateBMI(){
     else if(BMI <= 0){
         document.getElementById("showBMI").innerHTML = "ค่า BMI ของคุณคือ " + BMI.toFixed(2);
         document.getElementById("BMIValue").innerHTML = "เกิดข้อผิดพลาด ค่าที่ได้มีค่าติดลบ";
+        document.getElementById("BMIValue").style.color = "red";
+        document.getElementById("BMIValue").style.fontWeight = bold;
     }
     else if(isNaN(BMI)){
         document.getElementById("BMIValue").innerHTML = "เกิดข้อผิดพลาด";
+        document.getElementById("BMIValue").style.color = "red";
+        document.getElementById("BMIValue").style.fontWeight = bold;
     }
     console.log(Height);
     console.log(Weight);
