8a7fed0da92b4d2a274064d18439e5138c3e9d5a
CaptainPMP <scikung8@gmail.com>
2020-08-22 15:27:23 +0700

    add some text

--- app.js
+++ app.js
@@ -8,23 +8,23 @@ function CalculateBMI(){
     trueHeight = Height / 100;
     BMI = Weight / (trueHeight ** 2);
     if(BMI > 30){
-        document.getElementById("showBMI").innerHTML = "ค่า BMI ของคุณคือ " + BMI.toFixed(2);
+        document.getElementById("showBMI").innerHTML = "ค่า BMI ของคุณคือ " + BMI.toFixed(2) + " จากค่ามาตรฐานอาเซียน";
         document.getElementById("BMIValue").innerHTML = "คุณอยู่ในเกณฑ์โรคอ้วนระดับที่ 3";
     }
     else if(BMI > 25){
-        document.getElementById("showBMI").innerHTML = "ค่า BMI ของคุณคือ " + BMI.toFixed(2);
+        document.getElementById("showBMI").innerHTML = "ค่า BMI ของคุณคือ " + BMI.toFixed(2) + " จากค่ามาตรฐานอาเซียน";
         document.getElementById("BMIValue").innerHTML = "คุณอยู่ในเกณฑ์โรคอ้วนระดับที่ 2";
     }
     else if(BMI > 23){
-        document.getElementById("showBMI").innerHTML = "ค่า BMI ของคุณคือ " + BMI.toFixed(2);
+        document.getElementById("showBMI").innerHTML = "ค่า BMI ของคุณคือ " + BMI.toFixed(2) + " จากค่ามาตรฐานอาเซียน";
         document.getElementById("BMIValue").innerHTML = "คุณอยู่ในเกณฑ์โรคอ้วนระดับที่ 1";
     }
     else if(BMI >= 18.5){
-        document.getElementById("showBMI").innerHTML = "ค่า BMI ของคุณคือ " + BMI.toFixed(2);
+        document.getElementById("showBMI").innerHTML = "ค่า BMI ของคุณคือ " + BMI.toFixed(2) + " จากค่ามาตรฐานอาเซียน";
         document.getElementById("BMIValue").innerHTML = "คุณอยู่ในเกณฑ์ปกติ";
     }
     else if(BMI > 0){
-        document.getElementById("showBMI").innerHTML = "ค่า BMI ของคุณคือ " + BMI.toFixed(2);
+        document.getElementById("showBMI").innerHTML = "ค่า BMI ของคุณคือ " + BMI.toFixed(2) + " จากค่ามาตรฐานอาเซียน";
         document.getElementById("BMIValue").innerHTML = "คุณอยู่ในเกณฑ์น้ำหนักน้อยกว่ามาตรฐาน";
     }
     else if(BMI <= 0){
